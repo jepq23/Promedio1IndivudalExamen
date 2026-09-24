b0b0685e58e15327a841db855b6097189b933a52
Jesus <paredes.quispe23@gmail.com>
2026-09-24 17:21:07 -0500

    Jugador

---
+++ Promedio1Individual/Promedio1Individual/Jugador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio1Individual
+{
+    internal class Jugador
+    {
+        protected float vida;
+        protected float dmg;
+
+        public Jugador(float vida, float dmg)
+        {
+            this.vida = vida;
+            this.dmg = dmg;
+        }
+        public virtual float RecibirDano(float dmgRecibido)
+        {
+            return vida - dmgRecibido;
+        }
+        public virtual float AtaqueDMG(float dmg)
+        {
+            return dmg;
+        }
+    }
+}
